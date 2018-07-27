7691754bf84a50d9d9147910ed3f2cf9c101f5ee
Luca <luca.naterop@bluewin.ch>
2018-07-27 20:21:51 +0900

    git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Admin Dashboard for meteor react',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/lucnat/durian',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
