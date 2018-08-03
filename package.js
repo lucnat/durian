15c5da2125fd6ba78f74bd644b174b7c4addf906
Luca <luca.naterop@bluewin.ch>
2018-08-03 18:37:44 +0800

    version 0.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
   name: 'cul:durian',
-  version: '0.1.0',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Admin Dashboard for meteor react',
   // URL to the Git repository containing the source code for this package.
